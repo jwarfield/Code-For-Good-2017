9b7b3f4a57c31bf215ed6f4a5c04da6ccc2cc1b6
pablo <pablomd@uchicago.edu>
2017-09-29 23:16:57 -0500

    starting with partials

---
+++ public/assets/js/firebaseHelpers.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyB4v6TKAO8-75PwEcYHMxKbiEicZ5bv6RQ",
+    authDomain: "codeforgood15.firebaseapp.com",
+    databaseURL: "https://codeforgood15.firebaseio.com",
+    projectId: "codeforgood15",
+    storageBucket: "codeforgood15.appspot.com",
+    messagingSenderId: "1077718980229"
+  };
+firebase.initializeApp(config);
+
