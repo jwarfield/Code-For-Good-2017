960d44693c31cae8411cfae45ee2f0064cbde6f6
pablo <pablomd@uchicago.edu>
2017-09-30 06:06:03 -0500

    padding those commits

--- public/assets/js/firebaseImages.js
+++ public/assets/js/firebaseImages.js
@@ -4,6 +4,11 @@ query.once('value').then(function(snapshot){
 	addStudentImages(snapshot.val());
 });
 
+var query2 = firebase.database().ref('team-15/media')
+query2.once('value').then(function(snapshot){
+	addTeamImages(snapshot.val())
+})
+
 
 
 function addStudentImages(images){
@@ -12,4 +17,40 @@ function addStudentImages(images){
 		x.setAttribute('src',images[i]);
 		$('.my-photos')[0].appendChild(x);
 	}
+}
+
+function addTeamImages(images){
+	for(var i = 0; i < images.length; i++){
+		let x = document.createElement('img');
+		x.className = 'new-photo';
+		x.setAttribute('src',images[i]);
+		$('.choose-new-photos')[0].appendChild(x);
+	}
+	    $('.new-photo').click(function(e){
+				console.log('clicked');
+				const selected = e.target.getAttribute('selected');
+				if(selected !== null){
+					e.target.removeAttribute('selected')
+				}
+				else{
+					e.target.setAttribute('selected',true)
+				}
+			});
+}
+
+function setStudentImages(){
+	var selectedImages = [];
+	var possibleImages = $('.new-photo');
+	for(var i = 0; i < possibleImages.length; i++){
+		if(possibleImages[i].getAttribute('selected')){
+			selectedImages.push(possibleImages[i].getAttribute('src'));
+		}
+	}
+	console.log(selectedImages);
+	var ref = firebase.database().ref('team-15/students/0/media')
+	.on('value',function(snapshot){
+		var arr = snapshot.val().concat(selectedImages);
+		console.log(arr);
+		firebase.database().ref('team-15/students/0').child('media').set(arr);
+	})
 }
