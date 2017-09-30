f157363e08b52375e91f88eede2f65eeaf1accc0
Anya Chopra <anyachopra@Anyas-MacBook-Pro.local>
2017-09-29 22:51:49 -0400

    Website and server connected with stylings and images

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index.ejs', {title: 'Code for Good'})
 })
 
 app.listen(3000, function () {
